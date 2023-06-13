ec34097cd0b0ae2cc7a354e199529593e547870b
Aymen Sakouhi <aymenmarketer@gmail.com>
2023-06-13 20:44:31 +0200

    adding firebase connection and database and creating an items collection

--- app/page.js
+++ app/page.js
@@ -1,24 +1,64 @@
+//https://www.youtube.com/watch?v=uikATllLdRc&ab_channel=CodeCommerce
+//12:31
+
 "use client";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import {
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
 export default function Home() {
-  const [items, setItems] = useState([
-    {
-      name: "Food",
-      price: 4.99,
-    },
-    {
-      name: "Gas",
-      price: 3.99,
-    },
-    {
-      name: "Rent",
-      price: 1000,
-    },
-  ]);
+  const [items, setItems] = useState([]);
 
+  const [newItem, setNewItem] = useState({ name: "", price: "" });
   const [total, setTotal] = useState(0);
+
+  //Add item to database
+  const addItem = async (e) => {
+    e.preventDefault();
+    if (newItem.name !== "" || newItem.price !== "") {
+      setItems([...items, newItem]);
+    }
+    await addDoc(collection(db, "items"), {
+      name: newItem.name.trim(),
+      price: newItem.price.trim(),
+    });
+    setNewItem({ name: "", price: "" });
+  };
+
+  //Read items from database
+  //getting data on launch
+  useEffect(() => {
+    const q = query(collection(db, "items"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const itemArr = [];
+      querySnapshot.forEach((doc) => {
+        itemArr.push({ ...doc.data(), id: doc.id });
+      });
+      setItems(itemArr);
+      const calculateTotal = () => {
+        const totalPrice = itemArr.reduce(
+          (sum, item) => sum + parseFloat(item.price),
+          0
+        );
+        setTotal(totalPrice);
+      };
+      calculateTotal();
+      return () => unsubscribe();
+    });
+  }, []);
+
+  //Delete item from database
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, "items", id));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -29,16 +69,23 @@ export default function Home() {
             <input
               className="col-span-3 p-3 border"
               type="text"
-              placeholder="Enter text..."
+              placeholder="Enter Item..."
+              value={newItem.name}
+              onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
             />
             <input
               className="col-span-2 p-3 border mx-3"
               type="number"
               placeholder="Enter $..."
+              value={newItem.price}
+              onChange={(e) =>
+                setNewItem({ ...newItem, price: e.target.value })
+              }
             />
             <button
               className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl"
               type="submit"
+              onClick={addItem}
             >
               +
             </button>
@@ -48,11 +95,12 @@ export default function Home() {
               <li key={id} className="my-4 flex justify-between bg-slate-950">
                 <div className="p-4 w-full flex justify-between">
                   <span className="capitalize">{item.name}</span>
-                  <span>{item.price}</span>
+                  <span>$ {item.price}</span>
                 </div>
                 <button
                   className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16"
                   type="submit"
+                  onClick={() => deleteItem(item.id)}
                 >
                   X
                 </button>
@@ -60,14 +108,22 @@ export default function Home() {
             ))}
           </ul>
           {
-            <div className="flex justify-between p-4">
+            <div
+              className="flex 
+            justify-between
+            p-4"
+            >
               {total < 1 ? (
                 ""
               ) : (
-                <div>
-                  <span>Total</span>
-                  <span>$ {total}</span>
-                </div>
+                <>
+                  <div>
+                    <span>Total</span>
+                  </div>
+                  <div>
+                    <span>$ {total}</span>
+                  </div>
+                </>
               )}
             </div>
           }
